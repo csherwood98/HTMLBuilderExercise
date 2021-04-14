ba61b02f05976af1d5659a9c67c85f60690180ca
csherwood98 <zsherwood98@gmail.com>
2021-04-14 11:10:52 -0500

    Add project files.

---
+++ HTMLBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace HTMLBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello").AddChild("li", "world");
+            WriteLine(builder.ToString());
+        }
+    }
+}
